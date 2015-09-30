6ca27b9f63f5f118ed36d71c6382ca9ae688f327
mona <jalal@wisc.edu>
2015-09-30 14:25:35 -0500

    added the dataset

--- DTW.R
+++ DTW.R
@@ -4,7 +4,7 @@ set.seed(6218)
 n <- 10
 #sc <- read.table("/Users/mona/cs799/UCR_TS_Archive_2015/Gun_Point/Gun_Point_TRAIN")
 #sc <- read.table("~/cs799/UCR_TS/Gun_Point/Gun_Point_TRAIN", sep=",", stringsAsFactors=FALSE)
-sc <- read.table("synthetic_control.data", header=F, sep="")
+sc <- read.table("data/synthetic_control.data", header=F, sep="")
 s <- sample(1:100, n)
 idx <- c(s, 100+s, 200+s, 300+s, 400+s, 500+s)
 sample2 <- sc[idx, ]
