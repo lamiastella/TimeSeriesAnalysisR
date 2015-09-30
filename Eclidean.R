b0964c9b003364f99b9a8a3d6debaa0e622cbfb5
mona <jalal@wisc.edu>
2015-09-30 14:17:00 -0500

    Euclidean distance added

--- Eclidean.R
+++ Eclidean.R
@@ -1,4 +1,4 @@
-#DTW distance for hierarchical clustering of time series data 
+#Eclidean distance for hierarchical clustering of time series
 library(dtw)
 set.seed(6218)
 n <- 10
@@ -8,8 +8,7 @@ sc <- read.table("synthetic_control.data", header=F, sep="")
 s <- sample(1:100, n)
 idx <- c(s, 100+s, 200+s, 300+s, 400+s, 500+s)
 sample2 <- sc[idx, ]
-distance.matrix <- dist(sample2, method="DTW")
-hc <- hclust(distance.matrix, method="average")
+hc <- hclust(dist(sample2), method="average")
 observedLabels <- c(rep(1,n), rep(2,n), rep(3,n), rep(4,n), rep(5,n), rep(6,n))
 plot(hc, labels=observedLabels, main="")
 rect.hclust(hc, k=6)
